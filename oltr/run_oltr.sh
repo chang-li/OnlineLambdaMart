31aae1c48e10ab30d83f15da03e95299b0c47a65
Chang Li <cli1@ilps-h1.science.uva.nl>
2019-11-29 10:36:48 +0100

    update gitignore

--- oltr/run_oltr.sh
+++ oltr/run_oltr.sh
@@ -6,8 +6,8 @@
 #SBATCH -e ./out/%A_%a.err
 #SBATCH --time=24:00:00
 
-PYTHONPATH=/ivi/ilps/personal/cli1/OnlineLambdaMart
-python oltr/oltr_slurm.py
+PYTHONPATH=/ivi/ilps/personal/cli1/OnlineLambdaMart/oltr/
+python oltr_slurm.py
 # n_topic 5 10 19
 # n_pos 5 10
 # normalized: True False
